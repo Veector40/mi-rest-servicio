a4d8b7f3688abb9bf45735c76461a4e8801650fa
victorrobotxt <vkoychev@uni-sofia.bg>
2025-02-07 20:48:16 +0200

    Modify main.tf so successfully hook to the aws instance

--- main.tf
+++ main.tf
@@ -12,15 +12,52 @@ provider "aws" {
   region = var.region
 }
 
+# vars
 variable "region" {
   type    = string
   default = "eu-central-1"
 }
 
-resource "aws_security_group" "http_sg" {
-  name        = "allow-http-traffic"
-  description = "Allow inbound HTTP"
-  vpc_id      = var.vpc_id
+# If you have multiple subnets, specify one in the same VPC (vpc-0f58585ee0776273f).
+variable "subnet_id" {
+  type    = string
+  default = "subnet-002831f6079855201"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "key_pair_name" {
+  type    = string
+  default = "finalkey"
+}
+
+# Data sources
+
+data "aws_ami" "amazon_linux2023" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+# Resources
+resource "aws_security_group" "ssh_http" {
+  name        = "allow-ssh-http"
+  description = "Allow inbound SSH (22) and HTTP (80)"
+  vpc_id      = "vpc-0f58585ee0776273f"
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     description = "HTTP from anywhere"
@@ -31,7 +68,7 @@ resource "aws_security_group" "http_sg" {
   }
 
   egress {
-    description = "Outbound to anywhere"
+    description = "Allow all outbound"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -39,37 +76,21 @@ resource "aws_security_group" "http_sg" {
   }
 }
 
-variable "GHCR_PAT" {
-  description = "GitHub Container Registry Personal Access Token"
-  type        = string
-  sensitive   = true
-}
-
-resource "aws_instance" "my_ec2" {
-  ami                         = "ami-07eef52105e8a2059"
-  instance_type               = "t2.micro"
-  vpc_security_group_ids      = [aws_security_group.http_sg.id]
+resource "aws_instance" "al2023_instance" {
+  ami                         = data.aws_ami.amazon_linux2023.id
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [aws_security_group.ssh_http.id]
   associate_public_ip_address = true
+  key_name                    = var.key_pair_name
 
+  # Minimal user_data script (optional). Add any bootstrapping commands if needed.
   user_data = <<-EOF
     #!/bin/bash
     yum update -y
-    yum install docker -y
-    service docker start
-    usermod -aG docker ec2-user
-
-    docker login ghcr.io -u veector40 -p ${var.GHCR_PAT}
-    docker pull ghcr.io/veector40/mi-rest-servicio:latest
-    docker run -d -p 80:80 ghcr.io/veector40/mi-rest-servicio:latest
   EOF
 
   tags = {
-    Name = "my-simple-rest-service"
+    Name = "AmazonLinux2023-Terraform"
   }
-}
-
-
-variable "vpc_id" {
-  type    = string
-  default = "vpc-0f58585ee0776273f"
 }
